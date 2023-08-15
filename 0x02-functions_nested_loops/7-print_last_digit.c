a8b5fe1d29937bed0844901a6d3ad02b4ab9aba2
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-15 02:48:30 -0700

    last digit out!

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - prints last digit on integer
+ * @n: integer to pass through
+ *
+ * Return: @n last digit value
+ */
+int print_last_digit(int n)
+{
+	int d = abs(c % 10);
+	
+	_putchar(d + '0');
+	return (d);
+}
