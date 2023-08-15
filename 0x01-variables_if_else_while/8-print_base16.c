f7fc0a6836e26936cba3dd312007163aaf1f45e2
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-14 20:01:56 -0700

    HEXADECIMAL

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - code to print all digits of base16 num sys
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return(0);
+}
