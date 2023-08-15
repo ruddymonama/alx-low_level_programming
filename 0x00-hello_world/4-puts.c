6c318ff1747395948b579c008002fa1a72472892
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-14 18:33:38 -0700

    Script to use puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - ENtry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Program is like building a mutilingual puzzle");
+	return (0);
+}
