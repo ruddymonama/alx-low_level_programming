fada710096db23716aa8efba330f7362fe2b5d75
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-15 02:29:54 -0700

    is it a lower case?

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - determines if given letter is in lowercase
+ * @alpha: integer to be checked for lowercase
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int _islower(int alpha)
+{
+	if (alpha >= 97 && alpha < = 122)
+	{
+		return (1);
+	}
+	else
+	{
+		retrun(0);
+	}
+}
