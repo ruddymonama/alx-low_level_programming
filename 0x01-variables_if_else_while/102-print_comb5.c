f685b81561c759d837b4f09597712a467e86722a
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-14 20:39:19 -0700

    5 combos

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - code to print all possible combos of two 2-digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a,b;
+	/*enter conditionals*/
+	for (a = 0; a < 100; a++)
+	{
+		for (b = 0; b < 100; b++)
+		{
+			if (b > a)
+			{
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) +48);
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
