52165b2e64bec4fb0cad1d0d6c8989717dfea0af
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-15 03:04:21 -0700

    the ADDer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - summation of two integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: @c sum of a and b
+ */
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
