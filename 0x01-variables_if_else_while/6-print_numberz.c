43f5f001612ecaa6b33f1522cde36d159cfe6c95
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-14 19:53:23 -0700

    Numbers v1.1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - code that prints base10 numbers less than 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
