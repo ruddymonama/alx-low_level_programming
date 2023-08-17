ae725d76e06f6fef0b10c55ff8de65a1f699aa59
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-17 05:37:47 -0700

    testing Poz or Neg

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - test positive_or_negative function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d = 0;
+
+	positive_or_negative(d);
+	return (0);
+}
