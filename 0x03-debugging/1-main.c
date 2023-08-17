bbb631d25f496e4a8648f5f47fc34dead253dca4
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-17 06:33:46 -0700

    loop to infinityyyyy.......

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,10 +10,10 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 /*
- * 	while (i < 10)
- * 	{
- * 		putchar(i);
- * 	}
+ *	while (i < 10)
+ *	{
+ *		putchar(i);
+ *	}
  */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
