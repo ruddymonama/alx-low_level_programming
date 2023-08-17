0a918ee13d77188041f158e71ef5b752a271f778
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-17 06:26:28 -0700

    loop to infinityyyyyyy....

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - iterate infinite loop
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* int c = 0; */
+	printf("Infinite loop incoming :((\n");
+
+/*
+ * 	while (i < 10)
+ * 	{
+ * 		putchar(i);
+ * 	}
+ */
+	printf("Infinite loop deflected! \\0o0/\n");
+	return (0);
+}	
