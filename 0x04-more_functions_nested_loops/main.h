c241226058dd1bb7d914a081ca858262081f4d3a
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-22 08:25:36 -0700

    fix!

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * _putchar - prints the given char
- * @c: the char to be printed
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 
 int _putchar(char c);
 
