df2789bf0ec9e2fd5ac21e5a0100b45b8b30ab3d
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-25 05:56:35 -0700

    numberZ

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: NULL
+ */
+void print_numbers(void)
+{
+	int i = 0;
+	while (i <= 9)
+	{
+		putchar('0' + i);
+		i++;
+	}
+	_putchar('\n');
+}
