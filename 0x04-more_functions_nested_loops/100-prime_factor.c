17841fbbe4d323909a8e5b09f9bf3a8f2fad5276
Ruddy Monama <ruddythabang55@gmail.com>
2023-08-28 06:15:00 -0700

    fixed

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - print largest prime factor of 612852475143
  * 
  * Return: 0 on success
  */
-
 int main(void)
 {
 	unsigned long num = 612852475143;
@@ -19,7 +17,9 @@ int main(void)
 			divisor = 2;
 		}
 		else
+		{
 			divisor++;
+		}
 	}
 	printf("%lu\n", num);
 	return (0);
